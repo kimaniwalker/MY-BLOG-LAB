bcf2611e2e7cf9d26859e689d2f48286539d0075
Kimani Walker <kimaniwalker@gmail.com>
2018-07-09 15:35:39 -0500

    Cant get Input Box to show state

--- client/src/components/blogInput.jsx
+++ client/src/components/blogInput.jsx
@@ -5,10 +5,48 @@ class BlogInput extends Component {
     
     constructor(props) {
         super(props);
+        this.state = {
+            text:'',
+            hasLoaded: false
+        }
     }
 
+    hasLoaded = (e) => {
+        this.setState = ({
+            hasLoaded: !this.state.hasLoaded
+        });
+    }
+    componentDidMount = () => {
+        this.setState({hasLoaded: true})
+    }
+
+    onInputChange = (value) => {
+        this.setState({ text: value});
+    }
+
+
+
     render() {
-        return <input placeholder="Thoughts go here"></input>;
+
+        if (this.state.hasLoaded){
+          return 
+        <div>
+            <h1>{this.state.text}</h1>
+        <input 
+        value={this.state.text}
+         onChange={(event) => this.onInputChange (event.target.value)}
+        placeholder="Make Your Blog Post Now"/>
+        <button onClick={this.hasLoaded}>Submit</button>
+        </div>;  
+        } else {
+            return (
+                <div>
+                    <h1>LOADING....</h1>
+                    <button onClick = {this.hasLoaded}>Submit</button>
+                    </div>
+            )
+        }
+        
     }
 }
 
