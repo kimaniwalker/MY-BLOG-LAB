78e6514fe6b85ba745aa075bed0f63a96989704b
Kimani Walker <kimaniwalker@gmail.com>
2018-07-09 22:01:44 -0500

    Added BlogInput Component

--- client/src/components/blogList.jsx
+++ client/src/components/blogList.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import BlogInput from './blogInput';
 
 class BlogList extends Component {
     
@@ -9,6 +10,8 @@ class BlogList extends Component {
 
     render() {
         return (
+            <div>
+                <BlogInput />
             <div className="card">
   <div className="card-body">
     <h5 className="card-title">Card title</h5>
@@ -17,6 +20,7 @@ class BlogList extends Component {
     <a href="#" className="card-link">Card link</a>
     <a href="#" className="card-link">Another link</a>
   </div>
+</div>
 </div>
         );
     }
